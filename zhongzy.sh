5da3ce34b840f1dda5133248e6d9ca9b198cc961
zhongzy118 <zhongzy118@hotmail.com>
2021-09-12 23:59:37 +0800

    Update zhongzy.sh

--- zhongzy.sh
+++ zhongzy.sh
@@ -19,4 +19,4 @@ cat >> /etc/sudoers.d/zhongzy << "EOF"
 EOF
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 find / -name defaults.vim | xargs sed -i "s/set mouse=a/set mouse-=a/g" 
-systemctl restart sshd
+reboot
