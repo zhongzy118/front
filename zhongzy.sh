f3b8e71e87ae7b1d9645ec581e73c2e4594eca6d
zhongzy118 <zhongzy118@hotmail.com>
2021-09-12 22:34:46 +0800

    Create zhongzy.sh

---
+++ zhongzy.sh
@@ -0,0 +1,17 @@
+apt-get install sudo curl screen vim  htop wget -y 
+
+useradd -r -m -s /bin/bash zhongzy
+
+passwd zhongzy << "EOF"
+zhongzy123
+zhongzy123
+EOF
+
+cd /etc/sudoers.d
+touch zhongzy
+cat >> /etc/sudoers.d/zhongzy << "EOF"
+"zhongzy" ALL=(ALL) ALL
+EOF
+sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
+find / -name defaults.vim | xargs sed -i "s/set mouse=a/set mouse-=a/g" 
+systemctl restart sshd
