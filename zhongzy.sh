17669a5bd1b2deead59227f2b66efba62f567bf8
zhongzy118 <zhongzy118@hotmail.com>
2022-08-02 19:51:04 +0800

    Update zhongzy.sh

--- zhongzy.sh
+++ zhongzy.sh
@@ -20,13 +20,14 @@ cat >> /etc/sudoers.d/zhongzy << "EOF"
 EOF
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 find / -name defaults.vim | xargs sed -i "s/set mouse=a/set mouse-=a/g" 
+cls
 echo -n "please enter the hostname:"
 read NAME
 cat > /etc/hostname << "EOF"
-$NAME
+${NAME}
 EOF
 cat >> /etc/profile << "EOF"
-nohup /usr/bin/python3 /root/client-linux.py SERVER=mon.5119595.xyz USER=$NAME PASSWORD=zhongzy123 >/dev/null 2>&1 &
+nohup /usr/bin/python3 /root/client-linux.py SERVER=mon.5119595.xyz USER=${NAME} PASSWORD=zhongzy123 >/dev/null 2>&1 &
 EOF
 cd /root
 wget --no-check-certificate -qO client-linux.py 'https://raw.githubusercontent.com/cppla/ServerStatus/master/clients/client-linux.py' 
