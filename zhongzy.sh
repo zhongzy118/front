a20abdcaa546da79eaeb17a7536a0c74bc82ff61
zhongzy118 <zhongzy118@hotmail.com>
2022-08-02 23:00:54 +0800

    Update zhongzy.sh

--- zhongzy.sh
+++ zhongzy.sh
@@ -16,7 +16,7 @@ EOF
 cd /etc/sudoers.d
 touch zhongzy
 cat >> /etc/sudoers.d/zhongzy << "EOF"
-"zhongzy" ALL=(ALL) ALL
+"zhongzy" ALL=(ALL) NOPASSWD:ALL
 EOF
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 find / -name defaults.vim | xargs sed -i "s/set mouse=a/set mouse-=a/g" 
