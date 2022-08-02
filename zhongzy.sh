c331604f1d768ee264ad93842a29fecf07824850
zhongzy118 <zhongzy118@hotmail.com>
2022-08-02 20:08:04 +0800

    Update zhongzy.sh

--- zhongzy.sh
+++ zhongzy.sh
@@ -20,7 +20,7 @@ cat >> /etc/sudoers.d/zhongzy << "EOF"
 EOF
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 find / -name defaults.vim | xargs sed -i "s/set mouse=a/set mouse-=a/g" 
-cls
+clear
 echo -n "please enter the hostname:"
 read NAME
 echo ${NAME} > /etc/hostname
